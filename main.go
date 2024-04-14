a1245dc1eccaa79a9de0017a7846d6288d161cf5
Humanz <Just@Humanz.moe>
2024-04-15 01:42:18 +0700

    remove version

--- main.go
+++ main.go
@@ -3,27 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
-
 func main() {
-	version := getEnv("VERSION", "v.0.0.0")
 	// Register the handler function for the root ("/") route
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Write the response to the client
 		fmt.Fprint(w, "Hello, Humanz!")
-	})
-
-	http.HandleFunc("/version", func(w http.ResponseWriter, r *http.Request) {
-		// Write the response to the client
-		fmt.Fprint(w, "App version ", version)
 	})
 
 	if err := http.ListenAndServe(":2525", nil); err != nil {
